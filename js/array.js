4fef2a8cf2e3f4f0ffa7fb025ec39d49d7cf57ed
IsacHolstad <wisac22@gmail.com>
2022-06-17 20:28:53 +0200

    making car array

---
+++ js/array.js
@@ -0,0 +1,21 @@
+const cars = [{
+    name: "Mercedes-Benz",
+    year: 2019,
+    model: "E63s 4Matic",
+    horsepower: 660,
+    wheeldrive: "AWD",
+    fuel: "Gasoline",
+    milesprgallon: "5.6 gal/100mi"
+
+},
+{
+    name: "Bmw",
+    year: 2017,
+    model: "M3 Competition",
+    horsepower: 460,
+    wheeldrive: "RWD",
+    fuel: "Gasoline",
+    milesprgallon: "3.2 gal/100mi"
+}
+
+]
